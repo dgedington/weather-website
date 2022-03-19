d1d959b79d56785bd62e20f5f8ba65703078f6fb
Daniel Edington <dgeding@live.com>
2022-03-19 16:46:04 -0500

    Modiefied the app to improve stability

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geoCode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoic2luaXN0ZXJibGF5ZGUiLCJhIjoiY2wwcGplMG1rMjBydDNla2N5OWszcXA0ZCJ9.zqzRwLdREOAGDykaVBupWg&limit=1`
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2luaXN0ZXJibGF5ZGUiLCJhIjoiY2wwcGplMG1rMjBydDNla2N5OWszcXA0ZCJ9.zqzRwLdREOAGDykaVBupWg&limit=1'
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to geolocation service.', undefined)
